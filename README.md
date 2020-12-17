690913ef118858444aca54ab12d2a086b89144c9
jingping.ye@kingstarfintech.com <1104639669@qq.com>
2020-12-17 11:27:48 +0800

    feat:主体功能开发

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # u-secure-storage
-uni-app安全存储工具(vuex)
+
+uni-app 安全存储工具(vuex)
+
+## 使用指南
+
+> 假设存储持久化数据为 token 和 userInfo
+
+```js
+// store/index.js
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import USecureStorage from "u-secure-storage";
+const uSecureStorage = new USecureStorage();
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    userInfo: {},
+    token: "",
+  },
+  mutations: {
+    setToken(state, val) {
+      state.token = val;
+    },
+    setUserInfo(state, val) {
+      state.userInfo = val;
+    },
+  },
+  actions: {},
+  modules: {},
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => uSecureStorage.get(key),
+        setItem: (key, value) => uSecureStorage.set(key, value),
+        removeItem: (key) => uSecureStorage.remove(key),
+      },
+      paths: ["userInfo", "token"],
+    }),
+  ],
+});
+```
+
+## Q-A
+
+- 使用的加密方式
+
+使用的是`crypto-js`中的 AES 加密，同时使用了压缩。
---
+++ src/index.js
@@ -0,0 +1,87 @@
+import CryptoJS from "crypto-js";
+import AES from "crypto-js/aes";
+import LZString from "lz-string/libs/lz-string";
+import WordArray from "./WordArray";
+import PBKDF2 from "crypto-js/pbkdf2";
+
+class uSecureStorage {
+  constructor() {
+    this.uni = uni;
+    this.cipherKey = "";
+  }
+
+  // 获取数据
+  get(key) {
+    let data = "",
+      deCompressData,
+      decryptedData,
+      jsonData;
+    data = this.uni.getStorageSync(key);
+    deCompressData = LZString.decompressFromUTF16(data);
+    decryptedData = AES.decrypt(deCompressData, this.cipherKey).toString(CryptoJS.enc.Utf8);
+
+    if (!decryptedData) {
+      return data;
+    }
+
+    try {
+      jsonData = JSON.parse(decryptedData);
+    } catch (e) {
+      throw new Error("无法解析JSON！");
+    }
+
+    return jsonData;
+  }
+
+  // 产生密匙
+  generateCipherKey() {
+    let salt = WordArray.random(128 / 8);
+    let key128Bits = PBKDF2("36yiKsWEuHbGz@WH", salt, { keySize: 128 / 32 });
+    return key128Bits && key128Bits.toString();
+  }
+
+  //  处理数据
+  processData(data) {
+    if (!data === null || data === undefined || data === "") {
+      return "";
+    }
+
+    let jsonData, encodedData, compressedData;
+
+    try {
+      jsonData = JSON.stringify(data);
+    } catch (e) {
+      throw new Error("无法序列化JSON！");
+    }
+
+    // 加密数据
+    encodedData = AES.encrypt(jsonData, this.cipherKey).toString();
+
+    // 压缩数据
+    compressedData = LZString.compressToUTF16(encodedData);
+
+    return compressedData;
+  }
+
+  // 存数据
+  set(key, value) {
+    if (this.cipherKey.length === 0) {
+      this.cipherKey = this.generateCipherKey();
+    }
+    const strData = this.processData(value);
+    this.uni.setStorageSync(key, strData);
+  }
+
+  // 移除数据
+  remove(key) {
+    this.uni.removeStorageSync(key);
+  }
+
+  // 移除所有数据
+  clear() {
+    this.uni.clearStorageSync();
+  }
+}
+
+export default uSecureStorage;
+export { uSecureStorage };
